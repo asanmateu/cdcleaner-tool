e89b5d7efc984ab539e8cbe96f31143f7feb74c3
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-16 17:30:26 +0100

    Declared alert type constant variables

--- constants/alerts.py
+++ constants/alerts.py
@@ -1,7 +1,13 @@
-# Declaration of constant alert variables...
-
+# Alerts do not generate an omit but point out a possible error //////
 ALERT_TYPE = {
+    "sales_rep": "Non-existing sales rep: none was assigned, please submit an update request once setup; ",
+    "price_type": "",
+    "payment_method_setup": "Payment method not found, please set up and request update; ",
+    "payment_method_duplicate": "Payment method has duplicates, none has been defaulted. Please fix settings; ",
+    "shipping_method_setup": "Shipping method not found, please set up and request update; ",
+    "shipping_method_duplicate": "Payment method has duplicates, none has been defaulted. Please fix settings; ",
+    "email_missing": "No email address provided, no user will be created; ",
 
 }
 
-# Alerts do not generate an omit but point out a possible error...
+
