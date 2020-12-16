0e8b59feed5d51534beb9fcd4f9671758b43047d
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-16 17:57:01 +0100

    Adding city cleaner

--- services/city_cleaner.py
+++ services/city_cleaner.py
@@ -1,13 +1,16 @@
-
+# Import necessary modules...
+from errors import ERROR_TYPE
 
 
 def city_cleaner(data):
-    """
+    """ Note an error if city input is empty otherwise pass. """
 
-    Args:
-        data:
+    for i in range(0, len(data['City'])):
+        # Note error if value is empty otherwise leave as it is...
+        if data['City'].iloc[i] == '':
+            data['ERROR'].iloc[i] = str(data['ERROR'].iloc[i]) + ERROR_TYPE['city_error']
+        else:
+            pass
 
-    Returns:
-
-    """
+    return data
 
