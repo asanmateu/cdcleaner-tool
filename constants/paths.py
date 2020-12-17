8896e0a094bee2b72fd550954887894e5395da2f
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-17 20:58:15 +0100

    Declare input and output paths

---
+++ constants/paths.py
@@ -0,0 +1,6 @@
+PATHS = {
+    "input_path": r"/Users/antoniosanmateuserralta/Desktop/Joor-Projects/CD_automation/input",
+    "clean_output_path": r"/Users/antoniosanmateuserralta/Desktop/Joor-Projects/CD_automation/output/clean.csv",
+    "results_output_path": r"/Users/antoniosanmateuserralta/Desktop/Joor-Projects/CD_automation/output/results.csv"
+
+}
