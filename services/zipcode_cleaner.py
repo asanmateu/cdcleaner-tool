6bcab6a4c1d800fdbb9774c64d136dc5e2a0fb8a
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-17 13:47:33 +0100

    Designed zipcode cleaner

---
+++ services/zipcode_cleaner.py
@@ -0,0 +1,16 @@
+# Import necessary modules...
+from errors import ERROR_TYPE
+
+
+def zipcode_cleaner(data):
+    """ Notes an error if country is United States and zip code is empty, otherwise it does nothing.
+
+    Notes:
+        Run after cleaning countries and states.
+    """
+
+    for i in range(0, len(data['Zip'])):
+        if data['Country'].iloc[i] == 'United States' and data['Zip'].iloc[i] == "":
+            data['ERROR'].iloc[i] = str(data['ERROR'].iloc[i]) + ERROR_TYPE['zipcode_error']
+
+    return data
