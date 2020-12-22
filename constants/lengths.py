7cf207dd30e048455eae1c9758ee0c42f45ca9b6
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-22 19:33:03 +0100

    Define fields character limit constants

---
+++ constants/lengths.py
@@ -0,0 +1,17 @@
+# Character limits for validation fields...
+LIMITS = {
+    "customer_code": 100,
+    "customer_name": 150,
+    "address_code": 100,
+    "store_name": 255,
+    "address_1": 100,
+    "address_2": 100,
+    "city": 150,
+    "state": 40,
+    "zip": 20,
+    "phone": 30,
+    "buyer_name": 200,
+    "email": 200,
+    "price_currency": 3,
+    "discount": 3,
+}
