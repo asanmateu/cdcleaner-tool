b1aeb94af4c45d75e3605144f93011969f96180a
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-22 19:32:39 +0100

    Refactor if clause to and instead of or

--- services/callprod/salesrep_cleaner.py
+++ services/callprod/salesrep_cleaner.py
@@ -3,7 +3,7 @@ from alerts import ALERT_TYPE
 from queries import query_sales_reps
 
 
-def sales_rep_cleaner(data):
+def sales_rep_cleaner(data, designer_id: int):
     """ Calls sales reps query function and validates sales reps for each row of the customer list. If the sales rep
     does not exist it leaves the cell empty and leaves an alert note for csm to set it up an request an update.
 
@@ -17,7 +17,7 @@ def sales_rep_cleaner(data):
     # Iterate and clean sales reps while making alert note for those that don't exist...
     for i in range(len(data['Sales Rep'])):
         reference_sales_rep = data['Sales Rep'].iloc[i]
-        if reference_sales_rep not in sales_reps_dict.keys() or reference_sales_rep not in sales_reps_dict.values():
+        if (reference_sales_rep not in sales_reps_dict.keys()) and (reference_sales_rep not in sales_reps_dict.values()):
             data['ALERT'].iloc[i] = str(data['ALERT'].iloc[i]) + ALERT_TYPE["sales_rep"]
             data['Sales Rep'].iloc[i] = ""
 
