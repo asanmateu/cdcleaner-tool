4914ebed15593e1b86307ba806b67c2472eb76b8
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2020-12-24 14:03:37 +0100

    Refactor

--- dicts/addtypes.py
+++ dicts/addtypes.py
@@ -1,7 +1,10 @@
 # Contain valid address type and validation conversions
-valid_types = {'b': 'B',
-               's': 'S',
-               'billing': 'B',
-               'shipping': 'S',
-               'bs': 'BS',
-               '': 'BS'}
+valid_types = {
+    'b': 'B',
+    's': 'S',
+    'billing': 'B',
+    'shipping': 'S',
+    'bs': 'BS',
+    'b/s': 'BS',
+    '': 'BS'
+}
