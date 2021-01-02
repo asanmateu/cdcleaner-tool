02fcd34b4c85da099485ae8aac7fa5f054b619b6
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2021-01-02 15:58:56 +0100

    Add dict strip function

--- utils/myutils.py
+++ utils/myutils.py
@@ -1,4 +1,4 @@
-def trimmer(df_column):
-    """Trims leading and ending extra whitespaces of the selected field."""
+def strip_dict(d):
+    """ Removes whitespaces in dictionary key value pairs given an input dictionary. """
+    return {k.strip(): strip_dict(v) if isinstance(v, dict) else v.strip() for k, v in d.items()}
 
-    return
