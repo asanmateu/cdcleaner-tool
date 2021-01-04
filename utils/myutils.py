eb817a53cbf24a8a61ff985b93371dfb0b679dab
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2021-01-04 16:33:34 +0100

    add: new transformation utils

--- utils/myutils.py
+++ utils/myutils.py
@@ -2,3 +2,12 @@ def strip_dict(d):
     """ Removes whitespaces in dictionary key value pairs given an input dictionary. """
     return {k.strip(): strip_dict(v) if isinstance(v, dict) else v.strip() for k, v in d.items()}
 
+
+def df_to_object_type(df):
+    """ Converts columns in dataframe to object type. """
+    return df.apply(lambda x: x.astype(str))
+
+
+def strip_df(df):
+    """ Removes whitespaces in each dataframe object type column values. """
+    return df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
