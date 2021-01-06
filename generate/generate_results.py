bc7a2bf050512201465aa0775928377080df2b96
Toni Sanmateu <62403518+asanmateu@users.noreply.github.com>
2021-01-06 19:41:16 +0100

    add: comments

--- generate/generate_results.py
+++ generate/generate_results.py
@@ -2,7 +2,7 @@
 from results import RESULT_TYPE
 
 
-# Note fin
+# Note final result per row
 def generate_results(data):
     """ Populate results column in line with overall inputs in alert and error columns.
 
@@ -13,10 +13,13 @@ def generate_results(data):
     # Populate omissions with files containing errors...
     for i, row in data.iterrows():
         if row['ERROR'] != "":
+            # Files containing at least one error are denied
             data['RESULT'].iloc[i] = RESULT_TYPE['denied']
         elif row['ALERT'] != "":
+            # Files containing only alerts are defaulted but processed
             data['RESULT'].iloc[i] = RESULT_TYPE['defaulted']
         else:
+            # Files with no errors are approved to be processed
             data['RESULT'].iloc[i] = RESULT_TYPE['approved']
 
     return data
