85b3b4378b5a4a17e09fb62316088aa87766daaf
toni <antonio.sanmateu@gmail.com>
2021-01-11 21:07:36 +0100

    add: request designer id

--- executable.py
+++ executable.py
@@ -3,10 +3,10 @@ from pipeline import pipeline_master
 from paths import OUTPUT_PATH
 from library import os
 
-"""
+
 designer_id_value = input(DESIGNER + ": ")
 os.environ[DESIGNER] = designer_id_value
-"""
+
 # Validate environment variables input to check if their type and if any is null...
 
 
