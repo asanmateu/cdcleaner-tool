a946244a1bef94b7ad47050a0e88d0e27ef3d133
toni <antonio.sanmateu@gmail.com>
2021-01-11 20:24:30 +0100

    fix: to file dir

--- paths.py
+++ paths.py
@@ -1,7 +1,7 @@
 from library import os
 
 WORKING_DIR = os.getcwd()
-INPUT_PATH = WORKING_DIR + "/input"
-OUTPUT_PATH = WORKING_DIR + "/output"
-OUTPUT_CLEAN_FILE_PATH = WORKING_DIR + "/output/clean_file.csv"
-OUTPUT_RESULT_FILE_PATH = WORKING_DIR + "/output/results_file.csv"
+INPUT_PATH = WORKING_DIR + "/files/input"
+OUTPUT_PATH = WORKING_DIR + "/files/output"
+OUTPUT_CLEAN_FILE_PATH = WORKING_DIR + "/files/output/clean_file.csv"
+OUTPUT_RESULT_FILE_PATH = WORKING_DIR + "/files/output/results_file.csv"
