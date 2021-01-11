44b3bfea443392460a81f40ab4f4daf7784ecf4c
toni <antonio.sanmateu@gmail.com>
2021-01-11 22:46:19 +0100

    rm: no need to ask for env vars

--- executable.py
+++ executable.py
@@ -1,11 +1,7 @@
-from environment_constants import DESIGNER
 from pipeline import pipeline_master
 from paths import OUTPUT_PATH
 from library import os
 
-
-designer_id_value = input(DESIGNER + ": ")
-os.environ[DESIGNER] = designer_id_value
 
 # Validate environment variables input to check if their type and if any is null...
 
