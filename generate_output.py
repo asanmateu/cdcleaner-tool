f7c8efc9302da5682845533626223fcb14bd7d29
toni <antonio.sanmateu@gmail.com>
2021-01-11 22:47:19 +0100

    rm: no need for clean file

--- generate_output.py
+++ generate_output.py
@@ -1,6 +1,6 @@
 # Import necessary modules...
 from library import pd
-from paths import OUTPUT_CLEAN_FILE_PATH, OUTPUT_RESULT_FILE_PATH
+from paths import OUTPUT_RESULT_FILE_PATH
 from columns import ERROR
 
 
@@ -12,17 +12,7 @@ def generate_output(data):
         Run as the final function.
     """
 
-    clean_data = pd.DataFrame(columns=data.columns)
-
-    # Rescue all clean rows without error flags and send them to clean DataFrame...
-    for i, row in data.iterrows():
-        if row[ERROR] == "":
-            clean_data.append(row)
-
-    clean_data.drop(ERROR, axis=1, inplace=True)
-
     # Export files given output path and file name
-    clean_file = clean_data.to_csv(OUTPUT_CLEAN_FILE_PATH)
-    results_file = data.to_csv(OUTPUT_RESULT_FILE_PATH)
+    results_file = data.to_excel(OUTPUT_RESULT_FILE_PATH)
 
-    return clean_file, results_file
+    return results_file
