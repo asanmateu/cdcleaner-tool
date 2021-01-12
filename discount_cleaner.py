6b493819b3ee0e3e729e42fa5602d1cccc3615ae
toni <antonio.sanmateu@gmail.com>
2021-01-12 01:26:40 +0100

    add: discount length validation

--- discount_cleaner.py
+++ discount_cleaner.py
@@ -10,11 +10,10 @@ def discount_cleaner(data):
         # If there is any % symbol strip it...
         if row[DISCOUNT].count('%') > 1:
             data[DISCOUNT].iloc[i] = data[DISCOUNT].iloc[i].strip('%')
-    """
+
         # If length exceeds limit note an alert and remove the value...
         if len(str(row[DISCOUNT])) > LIMITS['discount']:
             data[ALERT].iloc[i] = str(row[ALERT]) + f"{row[DISCOUNT]}" + ALERT_TYPE['discount_length']
             data[DISCOUNT].iloc[i] = ""
-    """
 
     return data
