bb4bbfe2b9a44af05bb627a275f625844e20449c
toni <antonio.sanmateu@gmail.com>
2021-01-13 23:35:01 +0100

    fix __init__ files with edits

--- services/__init__.py
+++ services/__init__.py
@@ -8,3 +8,9 @@ from .customer_code_cleaner import customer_code_cleaner
 from .country_cleaner import country_cleaner
 from .address_cleaner import address_cleaner, address_type_cleaner, address_code_cleaner
 from .city_cleaner import city_cleaner
+from .customer_group_cleaner import customer_group_cleaner
+from .price_cleaner import price_type_cleaner
+from .company_number_cleaner import company_number_cleaner
+from .payment_cleaner import payment_methods_cleaner
+from .salesrep_cleaner import sales_rep_cleaner
+from .shipping_cleaner import shipping_methods_cleaner
