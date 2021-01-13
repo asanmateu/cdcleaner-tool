2c07e195367eed5a2d68bc0dd943a8937f621cdb
toni <antonio.sanmateu@gmail.com>
2021-01-13 23:36:12 +0100

    fix cyclic import error

--- prod/connection.py
+++ prod/connection.py
@@ -1,6 +1,29 @@
 # Import necessary modules from library...
-from constants import getpass, create_engine, exc, SSHTunnelForwarder, BaseSSHTunnelForwarderError, pd, \
-    CONNECTION_SETTINGS
+from constants import getpass, create_engine, exc, SSHTunnelForwarder, BaseSSHTunnelForwarderError, pd
+
+
+# Set up tunnel connection to prod with querying functions...
+CONNECTION_SETTINGS = {
+    'read_only_prod': {
+        'ssh_host': 'bastion.jooraccess.com',
+        'ssh_port': 22,
+        'remote_ip': 'localhost',
+        'remote_port': 7432,
+        'db_default': 'prodrds'},
+    'reporting': {
+        'ssh_host': 'www-reporting.joordev.com',
+        'ssh_port': 227,
+        'remote_ip': 'localhost',
+        'remote_port': 5432,
+        'db_default': 'joor_dev'},
+    'user': {
+        'name': getpass.getuser(),
+        'ssh_username': None,
+        'ssh_password': None,
+        'db_username': None,
+        'db_password': None,
+        'credentials_set': False}
+}
 
 
 def get_credentials():
