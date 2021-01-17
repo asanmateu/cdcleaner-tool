2da08df3dacb6f637a503a6328c7fda57f6f6667
toni <antonio.sanmateu@gmail.com>
2021-01-17 17:31:22 +0100

    add: error handling on template import

--- generate/generate_dataframe.py
+++ generate/generate_dataframe.py
@@ -1,4 +1,4 @@
-from constants import np, pd, os, INPUT_PATH, RESULT, ALERT, ERROR
+from constants import np, pd, os, sys, INPUT_PATH, RESULT, ALERT, ERROR
 from utils import strip_df, df_to_object_type
 
 
@@ -8,8 +8,17 @@ def generate_dataframe():
     # Change working directory to file inputs...
     os.chdir(INPUT_PATH)
 
-    # Load original excel sheet into a pandas DataFrame...
-    data = pd.read_excel("customer_list_cleaner_template.xlsx", na_values=['nan'])
+    try:
+        print("Reading and parsing template...")
+
+        # Load original excel sheet into a pandas DataFrame...
+        data = pd.read_excel("customer_list_cleaner_template.xlsx", na_values=['nan'])
+
+    except IndexError:
+        print("Error: make sure you use and .xlsx file that it is saved as Excel Workbook and NOT as "
+              "Strict Open XML Spreadsheet and try again.")
+
+        sys.exit()
 
     # Replace excel default null value with ""...
     data.replace('nan', np.nan, inplace=True)
