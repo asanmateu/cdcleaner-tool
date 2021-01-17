07e43ea4e0027e14e6c8307cde1affaeadb1b49c
toni <antonio.sanmateu@gmail.com>
2021-01-17 17:31:06 +0100

    add: print pipeline steps

--- pipeline.py
+++ pipeline.py
@@ -12,43 +12,74 @@ from prod import query_sales_reps, query_payment_methods, query_shipping_methods
 def pipeline_master(designer_id: int = DESIGNER_ID):
     """Call to each of the cleaning functions in the right order and return nothing"""
 
-    # Generate dataframe...
+    # Generate DataFrame...
     data = generate_dataframe()
 
     # Local validations...
+    print("Template successfully uploaded. Starting cleaning process: ")
+
     customer_code_cleaner(data)
+    print("Validating customer code column...")
     country_cleaner(data)
+    print("Validating country column...")
     state_cleaner(data)
+    print("Validating state column...")
     zipcode_cleaner(data)
+    print("Validating zip column...")
     address_type_cleaner(data)
+    print("Validating address type column...")
     address_cleaner(data)
+    print("Validating address 1 and address 2 columns...")
     address_code_cleaner(data)
+    print("Validating address codes...")
     city_cleaner(data)
+    print("Validating city column...")
     store_cleaner(data)
+    print("Validating store name column...")
     phone_cleaner(data)
+    print("Validating phone column...")
     email_cleaner(data)
+    print("Validating email column...")
     discount_cleaner(data)
+    print("Validating discounts column (beta)...\n")
 
     # Retrieve data from database...
+    print("Retrieving necessary data from prod: ")
+
     query_sales_reps(designer_id)
+    print("Retrieving sales reps...")
     query_payment_methods(designer_id)
+    print("Retrieving payment methods...")
     query_shipping_methods(designer_id)
+    print("Retrieving shipping methods...")
     query_price_types(designer_id)
+    print("Retrieving price types...")
     query_customer_groups(designer_id)
+    print("Retrieving customer groups...\n")
     query_company_numbers(designer_id)
 
     # Validation with db data...
+    print("Cleaning prod columns: ")
+
     sales_rep_cleaner(data, designer_id)
+    print("Validating sales reps...")
     payment_methods_cleaner(data, designer_id)
+    print("Validating payment methods...")
     shipping_methods_cleaner(data, designer_id)
+    print("Validating shipping methods...")
     price_type_cleaner(data, designer_id)
+    print("Validating price types...")
     customer_group_cleaner(data, designer_id)
+    print("Validating customer groups...")
     company_number_cleaner(data, designer_id)
+    print("Validating company numbers...\n")
 
     # Generate results flags...
     generate_results(data)
+    print("Generating result flags...\n")
 
     # Generate output files...
+    print("Generating ready file...\n")
     generate_output(data)
 
     return None
