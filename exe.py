6fba61885796a71f9211c71becaaacfe020531d1
toni <antonio.sanmateu@gmail.com>
2021-01-17 18:28:28 +0100

    add: exit upon completion

--- exe.py
+++ exe.py
@@ -1,4 +1,8 @@
 from pipeline import pipeline_master
+from constants import sys
 
 # Run pipeline master function to clean file and output results file...
 pipeline_master()
+
+# Exit once everything is done...
+sys.exit()
